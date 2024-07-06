6ab2567226d8b7b1216af244aa463d213b8c3117
Martín Pozo <mpozo@protonmail.com>
2024-07-06 18:57:30 +0200

    fix(nvim): syntax error in lsp config after Python changes

--- stow/nvim/.config/nvim/lua/plugins/lsp.lua
+++ stow/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -97,14 +97,6 @@ return {
           },
         },
       },
-      setup = {
-        [ruff] = function()
-          LazyVim.lsp.on_attach(function(client, _)
-            -- No disable hover in favor of Pyright
-            client.server_capabilities.hoverProvider = true
-          end, ruff)
-        end,
-      }
     },
   },
   {
