8840715c861d34a9062c4f1207ddafd6d74663ab
Martín Pozo <mpozo@protonmail.com>
2024-12-22 12:26:25 +0100

    fix(nvim): remove default snacks options

--- stow/nvim/.config/nvim/lua/plugins/snacks.lua
+++ stow/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,18 +2,6 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-      bigfile = { enabled = true },
-      quickfile = { enabled = true },
-      terminal = {
-        win = {
-          keys = {
-            nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
-            nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
-            nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
-            nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
-          },
-        },
-      },
       indent = {
         hl = {
           "DimmedRainbowYellow",
