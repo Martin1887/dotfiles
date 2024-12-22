c250ab4e7e5bca6be7faa444313be7069b29f4dc
Martín Pozo <mpozo@protonmail.com>
2024-12-22 12:05:11 +0100

    fix(nvim): remove persisted autocmd

--- stow/nvim/.config/nvim/lua/config/autocmds.lua
+++ stow/nvim/.config/nvim/lua/config/autocmds.lua
@@ -10,19 +10,6 @@ vim.cmd("hi NeoTreeGitUnstaged guifg=#DD8764")
 vim.cmd('autocmd BufEnter,BufWritePost,InsertLeave * lua require("lint").try_lint()')
 vim.cmd('autocmd BufEnter,BufWritePost,InsertLeave * lua require("lint").try_lint({"codespell"})')
 
-local group = vim.api.nvim_create_augroup("PersistedHooks", {})
-vim.api.nvim_create_autocmd({ "User" }, {
-  pattern = "PersistedTelescopeLoadPre",
-  group = group,
-  callback = function(session)
-    -- Save the currently loaded session
-    require("persisted").save({ session = vim.g.persisted_loaded_session })
-
-    -- Delete all of the open buffers
-    vim.api.nvim_input("<ESC>:%bd!<CR>")
-  end,
-})
-
 -- conceal disabled by default in Markdown and LaTeX, it is not very useful in
 -- TUI because many things are not correctly displayed
 vim.api.nvim_create_autocmd({ "FileType" }, {
