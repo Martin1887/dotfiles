90b980f0c697043bdf3aeac2e2da7217b6919c43
Martin1887 <mpozo@protonmail.com>
2025-04-02 16:57:07 +0200

    feat(nvim): save lazyvim.json in state instead of config

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -1,2 +1,5 @@
+-- lua/init.lua
+vim.g.lazyvim_json = vim.fn.stdpath("state") .. "/lazyvim.json"
+
 -- bootstrap lazy.nvim, Lazyvim and your plugins
 require("config.lazy")
--- stow/nvim/.config/nvim/lua/config/lazy.lua
+++ stow/nvim/.config/nvim/lua/config/lazy.lua
@@ -24,10 +24,6 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  news = {
-    lazyvim = false,
-    neovim = false,
-  },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
