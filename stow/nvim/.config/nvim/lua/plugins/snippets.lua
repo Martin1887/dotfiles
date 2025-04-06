59d5436e78b45d0ae4975833d8ec97d117239169
Martin1887 <mpozo@protonmail.com>
2025-04-06 11:22:10 +0200

    fix(nvim): nvim-scissors blink source and none jsonFormatter

--- stow/nvim/.config/nvim/lua/plugins/snippets.lua
+++ stow/nvim/.config/nvim/lua/plugins/snippets.lua
@@ -4,11 +4,31 @@ return {
     dependencies = { "garymjr/nvim-snippets" },
     opts = {
       snippetDir = vim.fn.stdpath("config") .. "/snippets",
-      jsonFormatter = "jq",
+      jsonFormatter = "none",
+      sources = {
+        providers = {
+          snippets = {
+            opts = {
+              search_paths = { vim.fn.stdpath("config") .. "/snippets" },
+            },
+          }
+        }
+      }
     },
     keys = {
       { "<leader>cpa", "<cmd>ScissorsAddNewSnippet<cr>", desc = "Add new snippet" },
       { "<leader>cpe", "<cmd>ScissorsEditSnippet<cr>",   desc = "Edit snippet" },
     },
   },
+  require("blink.cmp").setup {
+    sources = {
+      providers = {
+        snippets = {
+          opts = {
+            search_paths = { "path/to/your/snippetFolder" },
+          },
+        }
+      }
+    }
+  }
 }
