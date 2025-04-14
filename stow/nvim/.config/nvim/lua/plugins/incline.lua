8ac91d8fd84b1c37146983ea5dfa7dc6ebcfa856
Martin1887 <mpozo@protonmail.com>
2025-04-14 18:39:50 +0200

    fix(nvim): incline colors and non-focused window

--- stow/nvim/.config/nvim/lua/plugins/incline.lua
+++ stow/nvim/.config/nvim/lua/plugins/incline.lua
@@ -30,21 +30,28 @@ return {
           local filename = vim.fn.fnamemodify(bufname, ":t")
           local diagnostics = get_diagnostic_label(props)
           local modified = vim.api.nvim_buf_get_option(props.buf, "modified") and "bold,italic" or "None"
-          local filetype_icon, color, _is_default = require("mini.icons").get("file", filename)
+          local filetype_icon, color = require("nvim-web-devicons").get_icon_color(filename)
 
-          local buffer = {
-            { filetype_icon, hl = color },
-            { " " },
-            { filename,      gui = modified },
-          }
-
-          if #diagnostics > 0 and props.focused == true then
-            table.insert(diagnostics, { "| ", guifg = "grey" })
+          if props.focused == true then
+            local buffer = {
+              { filetype_icon, guifg = color },
+              { " " },
+              { filename,      guifg = color, gui = modified },
+            }
+            if #diagnostics > 0 then
+              table.insert(diagnostics, { "| ", guifg = "grey" })
+            end
+            for _, buffer_ in ipairs(buffer) do
+              table.insert(diagnostics, buffer_)
+            end
+            return diagnostics
+          else
+            return {
+              { filetype_icon, guifg = color },
+              { " " },
+              { filename,      guifg = "grey", gui = modified },
+            }
           end
-          for _, buffer_ in ipairs(buffer) do
-            table.insert(diagnostics, buffer_)
-          end
-          return diagnostics
         end,
       })
     end,
