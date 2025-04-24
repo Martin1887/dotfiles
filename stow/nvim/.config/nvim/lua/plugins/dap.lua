482706b8ca695ba2aafdd54189807ba99dbe614a
Martin1887 <mpozo@protonmail.com>
2025-04-24 09:43:01 +0200

    feat(nvim): ask for input file when launching clang debug

--- stow/nvim/.config/nvim/lua/plugins/dap.lua
+++ stow/nvim/.config/nvim/lua/plugins/dap.lua
@@ -21,4 +21,53 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      local dap = require("dap")
+      if not dap.adapters["codelldb"] then
+        require("dap").adapters["codelldb"] = {
+          type = "server",
+          host = "localhost",
+          port = "${port}",
+          executable = {
+            command = "codelldb",
+            args = {
+              "--port",
+              "${port}",
+            },
+          },
+        }
+      end
+      for _, lang in ipairs({ "c", "cpp" }) do
+        dap.configurations[lang] = {
+          {
+            type = "codelldb",
+            request = "launch",
+            name = "Launch file",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            stdio = { function()
+              local input = vim.fn.input("Path to input file: ", vim.fn.getcwd() .. "/", "file")
+              if not input == "" then
+                return input
+              else
+                return nil
+              end
+            end, nil, nil },
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "codelldb",
+            request = "attach",
+            name = "Attach to process",
+            pid = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+    end,
+  }
 }
